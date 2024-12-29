b5238244ac63a9f820a9308851dfb14b14c66327
Manjeet Singh <ms1857nit@gmail.com>
2024-12-29 17:51:52 +0530

    feat gradient main

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import styles from "./styles/stars.module.scss"
+import styles from "./styles/stars.module.scss";
+import gstyles from "./styles/gradient.module.scss";
 
 export default function Home() {
   return (
   <div>
-    <div className={styles.star}>Hello</div>
+    <div className={styles.star}></div>
+    <div className={gstyles.gradient}></div>
   </div>
 );
 }
